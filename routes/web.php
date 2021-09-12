6ada8cba2ec2ea6b3d726cb0511a7005849b8259
arief <arifnurabdullah03@gmail.com>
2021-09-12 10:20:30 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\pembayaran@mastersiswa');
 Route::get('/mastersiswa', 'App\Http\Controllers\pembayaran@mastersiswa');
 Route::get('/mastersiswa/delete/{id}', 'App\Http\Controllers\pembayaran@mastersiswadelete');
+Route::get('/mastersiswa/ubah/{id}', 'App\Http\Controllers\pembayaran@mastersiswaupdate');
+Route::post('/mastersiswa/ubah/', 'App\Http\Controllers\pembayaran@mastersiswaupdateaksi');
 Route::get('/masterkelas', 'App\Http\Controllers\pembayaran@masterkelas');
 Route::get('/masterkelas/delete/{id}', 'App\Http\Controllers\pembayaran@masterkelasdelete');
+Route::get('/masterkelas/ubah/{id}', 'App\Http\Controllers\pembayaran@masterkelasupdate');
+Route::post('/masterkelas/ubah/', 'App\Http\Controllers\pembayaran@masterkelasupdateaksi');
 Route::get('/masterspp', 'App\Http\Controllers\pembayaran@masterspp');
 Route::get('/masterspp/delete/{id}', 'App\Http\Controllers\pembayaran@mastersppdelete');
+Route::get('/masterspp/ubah/{id}', 'App\Http\Controllers\pembayaran@mastersppupdate');
+Route::post('/masterspp/ubah/', 'App\Http\Controllers\pembayaran@mastersppupdateaksi');
 Route::get('/mastermakanan', 'App\Http\Controllers\pembayaran@mastermakan');
 Route::get('/mastermakanan/delete/{id}', 'App\Http\Controllers\pembayaran@mastermakandelete');
+Route::get('/mastermakanan/ubah/{id}', 'App\Http\Controllers\pembayaran@mastermakanupdate');
+Route::post('/mastermakanan/ubah/', 'App\Http\Controllers\pembayaran@mastermakanupdateaksi');
 Route::get('/masterbiayakegiatan', 'App\Http\Controllers\pembayaran@masterkegiatan');
 Route::get('/masterbiayakegiatan/delete/{id}', 'App\Http\Controllers\pembayaran@masterkegiatandelete');
+Route::get('/masterbiayakegiatan/ubah/{id}', 'App\Http\Controllers\pembayaran@masterkegiatanupdate');
+Route::post('/masterbiayakegiatan/ubah/', 'App\Http\Controllers\pembayaran@masterkegiatanupdateaksi');
 Route::get('/masterbiayabukupaket', 'App\Http\Controllers\pembayaran@masterbuku');
 Route::get('/masterbiayabukupaket/delete/{id}', 'App\Http\Controllers\pembayaran@masterbukudelete');
+Route::get('/masterbiayabukupaket/ubah/{id}', 'App\Http\Controllers\pembayaran@masterbukuupdate');
+Route::post('/masterbiayabukupaket/ubah/', 'App\Http\Controllers\pembayaran@masterbukuupdateaksi');
 
 Route::get('datakeuangan', function () {
     return view('datakeuangan');
