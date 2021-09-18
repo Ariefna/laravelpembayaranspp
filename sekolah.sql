9b8d52f7ede9535363b2332102cb5ab69a1293bd
arief <arifnurabdullah03@gmail.com>
2021-09-18 08:21:10 +0700

    update db

--- sekolah.sql
+++ sekolah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Sep 2021 pada 05.31
+-- Waktu pembuatan: 18 Sep 2021 pada 03.20
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 7.4.16
 
@@ -39,7 +39,67 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id`, `id_semester`, `buku`, `harga`) VALUES
-(1, 1, 'matematika', 20000);
+(1, 1, 'matematika', 20000),
+(2, 1, 'aadx', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `harga_makan`
+--
+
+CREATE TABLE `harga_makan` (
+  `id` int(11) NOT NULL,
+  `id_semester` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `harga_makan`
+--
+
+INSERT INTO `harga_makan` (`id`, `id_semester`, `harga`) VALUES
+(1, 1, 500000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `harga_siswa`
+--
+
+CREATE TABLE `harga_siswa` (
+  `id` int(11) NOT NULL,
+  `nis` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `id_kelas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `harga_siswa`
+--
+
+INSERT INTO `harga_siswa` (`id`, `nis`, `nama`, `id_kelas`) VALUES
+(4, 232, 'ascsc', 1),
+(5, 233, 'scdvdv', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `harga_spp`
+--
+
+CREATE TABLE `harga_spp` (
+  `id` int(11) NOT NULL,
+  `id_semester` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `harga_spp`
+--
+
+INSERT INTO `harga_spp` (`id`, `id_semester`, `harga`) VALUES
+(1, 1, 2000);
 
 -- --------------------------------------------------------
 
@@ -58,7 +118,8 @@ CREATE TABLE `kegiatan` (
 --
 
 INSERT INTO `kegiatan` (`id`, `id_semester`, `harga`) VALUES
-(1, 1, 20000);
+(1, 1, 20001),
+(2, 1, 9000);
 
 -- --------------------------------------------------------
 
@@ -85,25 +146,6 @@ INSERT INTO `kelas` (`id`, `nama`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `makan`
---
-
-CREATE TABLE `makan` (
-  `id` int(11) NOT NULL,
-  `id_semester` int(11) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `makan`
---
-
-INSERT INTO `makan` (`id`, `id_semester`, `harga`) VALUES
-(1, 1, 50000);
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `semester`
 --
 
@@ -120,45 +162,6 @@ CREATE TABLE `semester` (
 INSERT INTO `semester` (`id`, `kode`, `nama`) VALUES
 (1, 211, 'Semester Genap 2021');
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `siswa`
---
-
-CREATE TABLE `siswa` (
-  `id` int(11) NOT NULL,
-  `nis` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `id_kelas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `siswa`
---
-
-INSERT INTO `siswa` (`id`, `nis`, `nama`, `id_kelas`) VALUES
-(2, 29873873, 'Rani', 2);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `spp`
---
-
-CREATE TABLE `spp` (
-  `id` int(11) NOT NULL,
-  `id_semester` int(11) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `spp`
---
-
-INSERT INTO `spp` (`id`, `id_semester`, `harga`) VALUES
-(1, 1, 200000);
-
 --
 -- Indexes for dumped tables
 --
@@ -167,6 +170,24 @@ INSERT INTO `spp` (`id`, `id_semester`, `harga`) VALUES
 -- Indeks untuk tabel `buku`
 --
 ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `harga_makan`
+--
+ALTER TABLE `harga_makan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `harga_siswa`
+--
+ALTER TABLE `harga_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `harga_spp`
+--
+ALTER TABLE `harga_spp`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -182,27 +203,9 @@ ALTER TABLE `kelas`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `makan`
---
-ALTER TABLE `makan`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indeks untuk tabel `semester`
 --
 ALTER TABLE `semester`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `siswa`
---
-ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `spp`
---
-ALTER TABLE `spp`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -213,13 +216,31 @@ ALTER TABLE `spp`
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `harga_makan`
+--
+ALTER TABLE `harga_makan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `harga_siswa`
+--
+ALTER TABLE `harga_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `harga_spp`
+--
+ALTER TABLE `harga_spp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `kegiatan`
 --
 ALTER TABLE `kegiatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelas`
@@ -228,27 +249,9 @@ ALTER TABLE `kelas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `makan`
---
-ALTER TABLE `makan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT untuk tabel `semester`
 --
 ALTER TABLE `semester`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `siswa`
---
-ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `spp`
---
-ALTER TABLE `spp`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
