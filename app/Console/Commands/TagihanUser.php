b38da33db45a1f9bde3532d7e6bd7f2c37225d19
arief <arifnurabdullah03@gmail.com>
2021-09-26 09:14:35 +0700

    update

--- app/Console/Commands/TagihanUser.php
+++ app/Console/Commands/TagihanUser.php
@@ -2,17 +2,17 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
-class tagihan extends Command
+class TagihanUser extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'demo:cron';
+    protected $signature = 'tagihan:users';
 
     /**
      * The console command description.
@@ -38,6 +38,7 @@ class tagihan extends Command
      */
     public function handle()
     {
+        DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT '1' as id_siswa, '0' as debet, harga_spp as kredit, CONCAT('Biaya SPP ',year(CURRENT_TIMESTAMP)) as keterangan FROM `spp` a join tahun b on a.id_tahun = b.id where b.kode = year(CURRENT_TIMESTAMP))");
         return 0;
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Console;
 
+
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -14,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        'App\Console\Commands\TagihanUser',
     ];
 
     /**
@@ -24,7 +27,15 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            Log::info('Cronjob berhasil dijalankan');
+        })->everyMinuteMinutes();
+        $schedule->command('tagihan:users')
+            ->everyMinuteMinutes();
+        // DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT '" . $id . "' as id_siswa, '0' as debet, harga_spp as kredit, CONCAT('Biaya SPP ',year(CURRENT_TIMESTAMP)) as keterangan FROM `spp` a join tahun b on a.id_tahun = b.id where b.kode = year(CURRENT_TIMESTAMP))");
         // $schedule->command('inspire')->hourly();
+        // Log::info('Cronjob berhasil dijalankan');
+        // })
     }
 
     /**
@@ -34,7 +45,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
