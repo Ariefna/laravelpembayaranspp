1942a2cd4cf63b310783a898910da55afe44511a
farhani <farhanikamilah@gmail.com>
2021-10-05 11:15:37 +0700

    Create spp_sekolah.sql

---
+++ spp_sekolah.sql
@@ -0,0 +1,355 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2021 at 06:15 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spp_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `nama_buku` varchar(100) NOT NULL,
+  `biaya_buku` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulan`
+--
+
+CREATE TABLE `bulan` (
+  `id` int(11) NOT NULL,
+  `nama_bulan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formulir`
+--
+
+CREATE TABLE `formulir` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_formulir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gedung`
+--
+
+CREATE TABLE `gedung` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_gedung` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_kegiatan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `les`
+--
+
+CREATE TABLE `les` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_les` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `makan`
+--
+
+CREATE TABLE `makan` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_makan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_kelas`
+--
+
+CREATE TABLE `master_kelas` (
+  `id` int(11) NOT NULL,
+  `nama_kelas` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_siswa`
+--
+
+CREATE TABLE `master_siswa` (
+  `id` int(11) NOT NULL,
+  `nis` bigint(100) NOT NULL,
+  `nama_siswa` varchar(100) NOT NULL,
+  `id_kelas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_tahun`
+--
+
+CREATE TABLE `master_tahun` (
+  `id` int(11) NOT NULL,
+  `kode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pakaian`
+--
+
+CREATE TABLE `pakaian` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_pakaian` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spp`
+--
+
+CREATE TABLE `spp` (
+  `id` int(11) NOT NULL,
+  `id_tahun` int(11) NOT NULL,
+  `biaya_spp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `biaya_formulir` int(11) NOT NULL,
+  `biaya_gedung` int(11) NOT NULL,
+  `biaya_pakaian` int(11) NOT NULL,
+  `biaya_buku` int(11) NOT NULL,
+  `biaya_makan` int(11) NOT NULL,
+  `biaya_spp` int(11) NOT NULL,
+  `biaya_les` int(11) NOT NULL,
+  `id_bulan` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bulan`
+--
+ALTER TABLE `bulan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `formulir`
+--
+ALTER TABLE `formulir`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gedung`
+--
+ALTER TABLE `gedung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `les`
+--
+ALTER TABLE `les`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `makan`
+--
+ALTER TABLE `makan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `master_kelas`
+--
+ALTER TABLE `master_kelas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `master_siswa`
+--
+ALTER TABLE `master_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `master_tahun`
+--
+ALTER TABLE `master_tahun`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pakaian`
+--
+ALTER TABLE `pakaian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `spp`
+--
+ALTER TABLE `spp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bulan`
+--
+ALTER TABLE `bulan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `formulir`
+--
+ALTER TABLE `formulir`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gedung`
+--
+ALTER TABLE `gedung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `les`
+--
+ALTER TABLE `les`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `makan`
+--
+ALTER TABLE `makan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `master_kelas`
+--
+ALTER TABLE `master_kelas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `master_siswa`
+--
+ALTER TABLE `master_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `master_tahun`
+--
+ALTER TABLE `master_tahun`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pakaian`
+--
+ALTER TABLE `pakaian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spp`
+--
+ALTER TABLE `spp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
