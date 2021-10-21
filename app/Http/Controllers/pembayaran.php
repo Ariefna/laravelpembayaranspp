a20ef8b47e19aa6ca7df5732333ed8d4fa0d97b2
AriefLinux <79347655+Ariefna@users.noreply.github.com>
2021-10-21 11:49:35 +0700

    Update pembayaran.php

--- app/Http/Controllers/pembayaran.php
+++ app/Http/Controllers/pembayaran.php
@@ -516,8 +516,11 @@ class pembayaran extends Controller
             }else if($data[0]->id_kelas != 6) {
                 DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT '" . $id . "' as id_siswa, '0' as debet, biaya_makan as kredit, CONCAT('Biaya makanan ',year(CURRENT_TIMESTAMP)) as keterangan FROM makan a join master_tahun b on a.id_tahun = b.id where b.kode = (select year(tanggal) from master_siswa where id = ".$id.") and status = 2)");
             }
+            if($data[0]->status_les != 0 && $data[0]->id_kelas != 6){
+        DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT ".$id." as id_siswa, '0' as debet, biaya_les as kredit, CONCAT('Biaya Les ',year(CURRENT_TIMESTAMP)) as keterangan FROM `les` a join master_tahun b on a.id_tahun = b.id where b.kode = year(CURRENT_TIMESTAMP) order by a.biaya_les desc limit 1)"); 
+              }
         DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT ".$id." as id_siswa, '0' as debet, biaya_spp as kredit, CONCAT('Biaya SPP ',year(CURRENT_TIMESTAMP)) as keterangan FROM `spp` a join master_tahun b on a.id_tahun = b.id where b.kode = year(CURRENT_TIMESTAMP))");
-        DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT ".$id." as id_siswa, '0' as debet, biaya_les as kredit, CONCAT('Biaya Les ',year(CURRENT_TIMESTAMP)) as keterangan FROM `les` a join master_tahun b on a.id_tahun = b.id where b.kode = year(CURRENT_TIMESTAMP) order by a.biaya_les desc limit 1)");
+      
         // DB::statement("INSERT INTO transaksi (id_siswa, debet, kredit, keterangan) (SELECT '" . $id . "' as id_siswa, '0' as debet, biaya_makan as kredit, CONCAT('Biaya makanan tahun',year(CURRENT_TIMESTAMP)) as keterangan FROM makan a join master_tahun b on a.id_tahun = b.id where b.kode = (year(CURRENT_TIMESTAMP)+1) and status = 2)");
         return redirect()->back()->with('success', 'Anda Berhasil Kirim Angsuran');
     }
