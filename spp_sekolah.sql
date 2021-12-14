ba5ad01d31eabd13b8e2adf39b83ccce21dddafe
farhani <farhanikamilah@gmail.com>
2021-12-14 20:56:47 +0700

    Update spp_sekolah.sql

--- spp_sekolah.sql
+++ spp_sekolah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2021 at 07:25 PM
+-- Generation Time: Dec 14, 2021 at 02:56 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 
@@ -24,44 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buku`
---
-
-CREATE TABLE `buku` (
-  `id` int(11) NOT NULL,
-  `id_tahun` int(11) NOT NULL,
-  `nama_buku` varchar(100) NOT NULL,
-  `biaya_buku` int(11) NOT NULL,
-  `id_kelas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `buku`
---
-
-INSERT INTO `buku` (`id`, `id_tahun`, `nama_buku`, `biaya_buku`, `id_kelas`) VALUES
-(1, 1, 'Buku Paket Kelas 1', 530000, 1),
-(2, 2, 'Buku Paket Kelas 1', 530000, 1),
-(4, 3, 'Buku Paket Kelas 1', 530000, 1),
-(5, 1, 'Buku Paket Kelas 2', 533200, 2),
-(6, 2, 'Buku Paket Kelas 2', 533200, 2),
-(7, 3, 'Buku Paket Kelas 2', 533200, 2),
-(8, 1, 'Buku Paket Kelas 3', 529200, 3),
-(9, 2, 'Buku Paket Kelas 3', 529200, 3),
-(10, 3, 'Buku Paket Kelas 3', 529200, 3),
-(11, 1, 'Buku Paket Kelas 4', 646000, 4),
-(12, 2, 'Buku Paket Kelas 4', 646000, 4),
-(13, 3, 'Buku Paket Kelas 4', 646000, 4),
-(14, 1, 'Buku Paket Kelas 5', 645200, 5),
-(15, 2, 'Buku Paket Kelas 5', 645200, 5),
-(16, 3, 'Buku Paket Kelas 5', 645200, 5),
-(17, 1, 'Buku Paket Kelas 6', 646800, 6),
-(18, 2, 'Buku Paket Kelas 6', 646800, 6),
-(20, 3, 'Buku Paket Kelas 6', 646800, 6);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `bulan`
 --
 
@@ -70,87 +32,23 @@ CREATE TABLE `bulan` (
   `nama_bulan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `formulir`
+-- Dumping data for table `bulan`
 --
 
-CREATE TABLE `formulir` (
-  `id` int(11) NOT NULL,
-  `id_tahun` int(11) NOT NULL,
-  `biaya_formulir` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `formulir`
---
-
-INSERT INTO `formulir` (`id`, `id_tahun`, `biaya_formulir`) VALUES
-(1, 1, 250000),
-(2, 2, 250000),
-(4, 3, 250000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `gedung`
---
-
-CREATE TABLE `gedung` (
-  `id` int(11) NOT NULL,
-  `id_tahun` int(11) NOT NULL,
-  `biaya_gedung` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `gedung`
---
-
-INSERT INTO `gedung` (`id`, `id_tahun`, `biaya_gedung`) VALUES
-(1, 1, 7000000),
-(2, 2, 7000000),
-(3, 3, 7000000),
-(4, 1, 9800000),
-(5, 2, 9800000),
-(6, 3, 9800000),
-(7, 1, 14000000),
-(8, 2, 14000000),
-(9, 3, 14000000),
-(10, 4, 1400000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kegiatan`
---
-
-CREATE TABLE `kegiatan` (
-  `id` int(11) NOT NULL,
-  `id_tahun` int(11) NOT NULL,
-  `biaya_kegiatan` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `kegiatan`
---
-
-INSERT INTO `kegiatan` (`id`, `id_tahun`, `biaya_kegiatan`, `status`) VALUES
-(1, 1, 1250000, 2),
-(2, 2, 1250000, 2),
-(3, 3, 1250000, 2),
-(4, 4, 1250000, 2),
-(5, 5, 1250000, 2),
-(6, 6, 1250000, 2),
-(7, 7, 1250000, 2),
-(8, 1, 1750000, 1),
-(9, 2, 1750000, 1),
-(10, 3, 1750000, 1),
-(11, 4, 1750000, 1),
-(12, 5, 1750000, 1),
-(13, 6, 1750000, 1),
-(15, 7, 1750000, 1);
+INSERT INTO `bulan` (`id`, `nama_bulan`) VALUES
+(1, 'January'),
+(2, 'February'),
+(3, 'maret'),
+(4, 'april'),
+(5, 'mei'),
+(6, 'juni'),
+(7, 'juli'),
+(8, 'agustus'),
+(9, 'september'),
+(10, 'oktober'),
+(11, 'november'),
+(12, 'desember');
 
 -- --------------------------------------------------------
 
@@ -171,10 +69,7 @@ CREATE TABLE `les` (
 INSERT INTO `les` (`id`, `id_tahun`, `biaya_les`) VALUES
 (1, 1, 100000),
 (2, 2, 100000),
-(3, 3, 100000),
-(5, 1, 0),
-(6, 2, 0),
-(7, 3, 0);
+(10, 20, 250000);
 
 -- --------------------------------------------------------
 
@@ -194,28 +89,11 @@ CREATE TABLE `makan` (
 --
 
 INSERT INTO `makan` (`id`, `id_tahun`, `biaya_makan`, `status`) VALUES
-(1, 1, 200000, 2),
-(2, 2, 200000, 2),
-(3, 3, 200000, 2),
-(5, 5, 200000, 2),
-(7, 4, 200000, 2),
-(8, 6, 200000, 2),
-(9, 7, 200000, 2),
-(10, 1, 2400000, 2),
-(11, 2, 2400000, 2),
-(12, 3, 2400000, 2),
-(13, 4, 2400000, 2),
-(14, 5, 2400000, 2),
-(15, 6, 2400000, 2),
-(16, 6, 2400000, 2),
-(17, 7, 2400000, 2),
-(18, 1, 0, 1),
-(19, 2, 0, 1),
-(20, 3, 0, 1),
-(21, 4, 0, 1),
-(22, 5, 0, 1),
-(23, 6, 0, 1),
-(24, 7, 0, 1);
+(1, 1, 2400000, 2),
+(2, 2, 2400000, 2),
+(16, 2, 0, 1),
+(17, 1, 0, 1),
+(21, 20, 2500000, 2);
 
 -- --------------------------------------------------------
 
@@ -238,7 +116,7 @@ INSERT INTO `master_kelas` (`id`, `nama_kelas`) VALUES
 (3, 'Kelas 3'),
 (4, 'Kelas 4'),
 (5, 'Kelas 5'),
-(6, 'Kelas 6');
+(13, 'Kelas 6');
 
 -- --------------------------------------------------------
 
@@ -248,10 +126,12 @@ INSERT INTO `master_kelas` (`id`, `nama_kelas`) VALUES
 
 CREATE TABLE `master_siswa` (
   `id` int(11) NOT NULL,
+  `foto_siswa` text NOT NULL,
   `nis` bigint(100) NOT NULL,
   `nama_siswa` varchar(100) NOT NULL,
   `id_kelas` int(11) NOT NULL,
   `status_les` int(11) NOT NULL,
+  `status_makan` int(11) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -259,21 +139,31 @@ CREATE TABLE `master_siswa` (
 -- Dumping data for table `master_siswa`
 --
 
-INSERT INTO `master_siswa` (`id`, `nis`, `nama_siswa`, `id_kelas`, `status_les`, `tanggal`) VALUES
-(1, 1841012225, 'Coba', 1, 0, '2021-10-15 13:57:02'),
-(2, 18452878, 'Coba 2', 2, 0, '2021-10-15 13:57:02'),
-(3, 184101022, 'Coba 3', 3, 0, '2021-10-15 13:57:02'),
-(4, 1845461, 'Coba 4', 4, 0, '2021-10-15 13:57:02'),
-(5, 18410225, 'Coba 5', 5, 0, '2021-10-15 13:57:02'),
-(7, 1000000, 'Coba 6', 6, 0, '2021-10-15 13:57:02'),
-(9, 1872465, 'Coba 11', 1, 0, '2021-10-15 13:57:02'),
-(10, 154841323, 'Coba 22', 2, 0, '2021-10-15 13:57:02'),
-(11, 1574562632, 'Coba 33', 3, 0, '2021-10-15 13:57:02'),
-(12, 13889566, 'Coba 44', 4, 0, '2021-10-15 13:57:02'),
-(13, 13854664, 'Coba 55', 5, 0, '2021-10-15 13:57:02'),
-(14, 158461616, 'Coba 66', 6, 0, '2021-10-15 13:57:02'),
-(15, 121566, 'Hasil', 1, 1, '2021-10-15 17:09:01'),
-(16, 154653131, 'Hasil', 1, 1, '2021-10-15 17:10:36');
+INSERT INTO `master_siswa` (`id`, `foto_siswa`, `nis`, `nama_siswa`, `id_kelas`, `status_les`, `status_makan`, `tanggal`) VALUES
+(206, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 307, 'Abraruz Sakha Kyanda Putera', 1, 0, 0, '2021-12-08 15:39:56'),
+(207, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 308, 'Adeola Syavira', 1, 0, 1, '2021-12-08 15:40:38'),
+(208, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 309, 'Ahmad Hisyam Ahza Abdillah', 1, 0, 0, '2021-12-08 15:41:42'),
+(209, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 310, 'Aisyah As-Shofiyyah Fillah', 1, 0, 0, '2021-12-08 15:42:19'),
+(210, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 311, 'Al-Dzaki Giri Satya', 1, 1, 0, '2021-12-08 15:42:53'),
+(211, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 312, 'Al-Ghazali', 1, 0, 0, '2021-12-08 15:43:20'),
+(212, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 313, 'Dafarel Abiyan Al-Farizi', 1, 0, 0, '2021-12-08 15:43:54'),
+(213, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 314, 'Ghassani Fakhirah Althaf Sahifah', 1, 1, 1, '2021-12-08 15:44:39'),
+(214, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 315, 'Hafidz Maulana', 1, 0, 1, '2021-12-08 15:45:19'),
+(215, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 316, 'Hanif Al-Fauzan', 1, 0, 1, '2021-12-08 15:45:54'),
+(216, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 317, 'Ismail Uwais Assalafy', 1, 1, 0, '2021-12-08 15:46:24'),
+(217, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 318, 'Jaisyi Muhammad', 1, 0, 1, '2021-12-08 15:46:58'),
+(218, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 319, 'Karissa Nur Muharromah Y.', 1, 0, 0, '2021-12-08 15:47:26'),
+(219, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 320, 'King Uwais AL-Qorni', 1, 0, 0, '2021-12-08 15:47:54'),
+(220, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 321, 'M. Althaf Muzakky', 1, 0, 0, '2021-12-08 15:48:19'),
+(221, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 322, 'M. Faiz Ramadhan', 1, 1, 0, '2021-12-08 15:48:51'),
+(222, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 323, 'M. Naf\'an Ubaidillah', 1, 0, 0, '2021-12-08 15:49:20'),
+(223, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 324, 'M. Ubaidillah', 1, 1, 0, '2021-12-08 15:49:52'),
+(224, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 325, 'Shafiyah Amirah Yumna', 1, 0, 0, '2021-12-08 15:50:21'),
+(226, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 326, 'Sultan Zahfran Li Priambodo', 1, 0, 0, '2021-12-08 15:55:32'),
+(245, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 457, 'Aisyah', 6, 1, 1, '2021-12-14 11:19:14'),
+(246, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 122, 'ananda', 6, 1, 1, '2022-12-14 04:24:42'),
+(247, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 122, 'Aisyah', 6, 1, 1, '2021-12-14 11:36:52'),
+(249, 'png-transparent-logo-person-user-person-icon-rectangle-photography-computer-wallpaper.png', 321, 'Aisyah', 1, 1, 1, '2021-12-14 13:54:44');
 
 -- --------------------------------------------------------
 
@@ -293,32 +183,7 @@ CREATE TABLE `master_tahun` (
 INSERT INTO `master_tahun` (`id`, `kode`) VALUES
 (1, 2022),
 (2, 2021),
-(3, 2020),
-(4, 2019),
-(5, 2018),
-(6, 2017),
-(7, 2016);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pakaian`
---
-
-CREATE TABLE `pakaian` (
-  `id` int(11) NOT NULL,
-  `id_tahun` int(11) NOT NULL,
-  `biaya_pakaian` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pakaian`
---
-
-INSERT INTO `pakaian` (`id`, `id_tahun`, `biaya_pakaian`) VALUES
-(1, 1, 500000),
-(2, 2, 500000),
-(3, 3, 500000);
+(24, 2023);
 
 -- --------------------------------------------------------
 
@@ -339,11 +204,7 @@ CREATE TABLE `spp` (
 INSERT INTO `spp` (`id`, `id_tahun`, `biaya_spp`) VALUES
 (1, 1, 950000),
 (2, 2, 850000),
-(3, 3, 850000),
-(4, 4, 825000),
-(5, 5, 800000),
-(6, 6, 700000),
-(7, 7, 790000);
+(11, 20, 985000);
 
 -- --------------------------------------------------------
 
@@ -354,65 +215,22 @@ INSERT INTO `spp` (`id`, `id_tahun`, `biaya_spp`) VALUES
 CREATE TABLE `transaksi` (
   `id` int(11) NOT NULL,
   `id_siswa` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
   `debet` int(11) NOT NULL,
   `kredit` int(11) NOT NULL,
-  `keterangan` text NOT NULL
+  `keterangan` text NOT NULL,
+  `bukti` text DEFAULT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `transaksi`
---
-
-INSERT INTO `transaksi` (`id`, `id_siswa`, `debet`, `kredit`, `keterangan`) VALUES
-(1, 1, 0, 850000, 'Biaya SPP 2021'),
-(2, 1, 0, 850000, 'Biaya SPP 2021'),
-(3, 1, 1700000, 0, 'Uang kertas formulir'),
-(4, 1, 0, 850000, 'Biaya SPP 2021'),
-(5, 2, 0, 850000, 'Biaya SPP 2021'),
-(6, 2, 850000, 0, 'Uang kertas formulir'),
-(7, 1, 0, 850000, 'Biaya SPP 2021'),
-(8, 3, 0, 850000, 'Biaya SPP 2021'),
-(9, 4, 0, 850000, 'Biaya SPP 2021'),
-(10, 1, 1700000, 0, 'semua'),
-(11, 1, 0, 850000, 'Biaya SPP 2021'),
-(12, 2, 0, 850000, 'Biaya SPP 2021'),
-(13, 3, 0, 850000, 'Biaya SPP 2021'),
-(14, 4, 0, 850000, 'Biaya SPP 2021'),
-(15, 5, 0, 850000, 'Biaya SPP 2021'),
-(16, 7, 0, 850000, 'Biaya SPP 2021');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `buku`
---
-ALTER TABLE `buku`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `bulan`
 --
 ALTER TABLE `bulan`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `formulir`
---
-ALTER TABLE `formulir`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `gedung`
---
-ALTER TABLE `gedung`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `kegiatan`
---
-ALTER TABLE `kegiatan`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -446,12 +264,6 @@ ALTER TABLE `master_tahun`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `pakaian`
---
-ALTER TABLE `pakaian`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `spp`
 --
 ALTER TABLE `spp`
@@ -468,82 +280,52 @@ ALTER TABLE `transaksi`
 --
 
 --
--- AUTO_INCREMENT for table `buku`
---
-ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
 -- AUTO_INCREMENT for table `bulan`
 --
 ALTER TABLE `bulan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `formulir`
---
-ALTER TABLE `formulir`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `gedung`
---
-ALTER TABLE `gedung`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `kegiatan`
---
-ALTER TABLE `kegiatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `les`
 --
 ALTER TABLE `les`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `makan`
 --
 ALTER TABLE `makan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `master_kelas`
 --
 ALTER TABLE `master_kelas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `master_siswa`
 --
 ALTER TABLE `master_siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=250;
 
 --
 -- AUTO_INCREMENT for table `master_tahun`
 --
 ALTER TABLE `master_tahun`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `pakaian`
---
-ALTER TABLE `pakaian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `spp`
 --
 ALTER TABLE `spp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1612;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
